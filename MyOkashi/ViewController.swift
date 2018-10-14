da0f0620edd745f54cf3a8d933f797ca2dea5ae7
Kyohei Yamaguchi <kyohei3235@gmail.com>
2018-10-14 20:32:50 +0900

    initial commit

---
+++ MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by Kyohei Yamaguchi on 2018/10/14.
+//  Copyright © 2018年 Kyohei Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
