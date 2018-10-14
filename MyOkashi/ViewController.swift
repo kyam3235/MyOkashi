494f68b00e35c01d61f2720626753f2036cd604f
Kyohei Yamaguchi <kyohei3235@gmail.com>
2018-10-14 21:05:45 +0900

    検索ボタンタップ時にSearchBarに入力されたテキストをデバッグ出力する機能を追加

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //SearchBarのDelegate通知先を設定
+        searchText.delegate = self
+        //入力のヒントになるプレースホルダを設定
+        searchText.placeholder = "お菓子の名前を入力してください"
     }
 
     @IBOutlet weak var searchText: UISearchBar!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    //検索ボタンをタップ
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //キーボードを閉じる
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text{
+            //デバッグエリアに出力
+            print(searchWord)
+        }
+    }
 }
 
